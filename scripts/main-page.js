e935eb64e5fbad1b9943b553cdc1cc201211fbc3
Samrud Reddy <samrudr@gmail.com>
2023-08-11 18:37:12 +0530

    Carasel without animation is done

--- scripts/main-page.js
+++ scripts/main-page.js
@@ -1,52 +1,45 @@
 var slide_pos = 1;
 const number_of_slides = $('.slideshow').length;
+const rem = 16;
 
 for (let i = 1; i<=number_of_slides; i++){
-    $(`.selector`).append(`<span class="dot" onclick="set_slide(${i})"></span>`);
-}
-
-function hide_all_slides(){
-    for (let i = 1; i<=number_of_slides; i++){
-        $(`.images>.slideshow:nth-child(${i})`).hide();
-        $(`.dot:nth-child(${i})`).removeClass("selected");
-    }
-}
-
-function update_slides(){
-    hide_all_slides();
-
-    $(`.images>.slideshow:nth-child(${slide_pos})`).show();
-    $(`.dot:nth-child(${slide_pos})`).addClass("selected");
+    $(`.selector`).append(`<span class="dot" onclick="setSlidePos(${i})"></span>`);
 }
 
 function mod(n, m) {
     return ((n % m) + m) % m;
-}
+  }
 
-function change_slides(x){
-    let fixed_slide_pos = slide_pos-1;
-    x = fixed_slide_pos + x;
 
-    x = mod(x, number_of_slides);
-
-    x = x + 1;
-
+function setSlidePos(x){
     slide_pos = x;
-    update_slides();
+    let width_of_img = $(window).width() - (rem * 7);
+    let fixed_x = mod((x-1), (number_of_slides))
+   
+    // let start_pos = $(".carasel").scrollLeft();
+    let final_scroll = fixed_x*width_of_img;
+    // let scroll_pos = start_pos;
+    // let scroll_len = final_scroll - start_pos;
+
+
+    $(".carasel").scrollLeft(final_scroll);
 }
 
-function set_slide(x){
-    slide_pos = x;
-    update_slides();
-}
 
-update_slides();
+
+setSlidePos(slide_pos)
+
+$(window).resize(function() {
+    setSlidePos(slide_pos);
+})
 
 $(document).keydown(function(e) {
     if (e.which === 39) {
-        change_slides(1);
+        slide_pos = mod(((slide_pos + 1)-1), number_of_slides)+1;
+        setSlidePos(slide_pos);
     }
     else if (e.which === 37) {
-        change_slides(-1);
+        slide_pos = mod(((slide_pos - 1)-1), number_of_slides)+1;
+        setSlidePos(slide_pos);
     }
 })
