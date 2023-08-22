1a892d61ab34cf2a29080d59c5250741625e76ad
Samrud Reddy <samrudr@gmail.com>
2023-08-22 21:51:59 +0530

    Finished the scroll animations for the headers

--- scripts/scroll-reveal.js
+++ scripts/scroll-reveal.js
@@ -1,10 +1,19 @@
 function onScroll(){
-    var revealPoint = 150;
+    var revealPoint = 0;
 
-    let apear = $(".website-section");
-    for (i = 0; i<apear.length; i+=1){
-        
+    let toApear = document.querySelectorAll(".website-section");
+    let windowHeight = window.innerHeight;
+    console.log(windowHeight)
+    for (i = 0; i<toApear.length; i+=1){
+
+        let revealTop = toApear[i].getBoundingClientRect().top;
+        if (revealTop < windowHeight - revealPoint) {
+			toApear[i].classList.add("active");
+		} else {
+			toApear[i].classList.remove("active");
+		}
     }
 }
+onScroll()
 
 $(window).scroll(onScroll)
