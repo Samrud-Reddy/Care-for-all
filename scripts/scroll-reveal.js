03db602ab4df2a5b143520ba93a689727af0ca71
Samrud Reddy <samrudr@gmail.com>
2023-08-24 23:08:16 +0530

    Time line done

--- scripts/scroll-reveal.js
+++ scripts/scroll-reveal.js
@@ -1,19 +1,86 @@
-function onScroll(){
-    var revealPoint = 0;
+const counters = document.querySelectorAll('.counter');
 
-    let toApear = document.querySelectorAll(".website-section");
-    let windowHeight = window.innerHeight;
-    console.log(windowHeight)
-    for (i = 0; i<toApear.length; i+=1){
+function animate(elem, progress) {
+    let final = +elem.getAttribute("final")
+    let time = +elem.getAttribute("time")*100/5
 
-        let revealTop = toApear[i].getBoundingClientRect().top;
-        if (revealTop < windowHeight - revealPoint) {
-			toApear[i].classList.add("active");
-		} else {
-			toApear[i].classList.remove("active");
-		}
+    let interval = final/time
+
+
+    if (progress < final){
+        progress = progress+interval;
+        if (progress>final){
+            progress=final;
+        }
+        elem.innerHTML = Math.floor(progress);
+        setTimeout(animate, 50, elem, progress)
     }
 }
-onScroll()
 
-$(window).scroll(onScroll)
+function counters_hide(x){
+    if (x){
+        $(".counter").css("opacity", "0%")
+    }
+    else {
+        $(".counter").css("opacity", "100%")
+    }
+}
+
+function start_animation(){
+    counters_hide(false)
+    animation_run = true;
+    counters.forEach( counter => {
+        animate(counter, 0)
+    });
+}
+
+counters_hide(true);
+animation_run = false;
+
+function scroll_handler(){
+    let top = document.getElementsByClassName("counters")[0].getBoundingClientRect().top;
+    let bottom = document.getElementsByClassName("counters")[0].getBoundingClientRect().bottom;
+
+    let window_hieght = window.innerHeight
+    let offset = 100;
+
+    if (window_hieght > (bottom)){
+        if (!animation_run){
+            start_animation()
+        }
+    }
+    else if(window_hieght<top){
+        counters_hide(true)
+        animation_run = false;
+    }
+}
+
+$(window).scroll(scroll_handler)
+
+$(function(){
+	var inputs = $('.input');
+	var paras = $('.description-flex-container').find('p');
+	inputs.click(function(){
+		var t = $(this),
+				ind = t.index(),
+				matchedPara = paras.eq(ind);
+		
+		t.add(matchedPara).addClass('active');
+		inputs.not(t).add(paras.not(matchedPara)).removeClass('active');
+	});
+});
+
+
+$(function(){
+	var inputs = $('.not_end');
+	var paras = $('.descripters').find('p');
+
+	inputs.click(function(){
+		var t = $(this),
+				ind = t.index()-1,
+				matchedPara = paras.eq(ind);
+		
+		t.add(matchedPara).addClass('active');
+		inputs.not(t).add(paras.not(matchedPara)).removeClass('active');
+	});
+});
