2919e4157bf161e55744ecc927e755f56307162f
Samrud Reddy <samrudr@gmail.com>
2023-08-30 18:16:03 +0800

    removed dead code

--- scripts/scroll-reveal.js
+++ scripts/scroll-reveal.js
@@ -55,20 +55,11 @@ function scroll_handler(){
     }
 }
 
-$(window).scroll(scroll_handler)
+$(document).ready(scroll_handler)
 
-$(function(){
-	var inputs = $('.input');
-	var paras = $('.description-flex-container').find('p');
-	inputs.click(function(){
-		var t = $(this),
-				ind = t.index(),
-				matchedPara = paras.eq(ind);
-		
-		t.add(matchedPara).addClass('active');
-		inputs.not(t).add(paras.not(matchedPara)).removeClass('active');
-	});
-});
+
+
+
 
 
 $(function(){
