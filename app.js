62c4aea082fa136ae4e0d05ceb3d25ee54ab4f13
Samrud Reddy <samrudr@gmail.com>
2024-05-03 22:45:16 +0530

    made all the share links work

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.use((req, res) => {
   res.render("pagenotfound.ejs")
 })
 
-app.listen(port, () => {
+app.listen(port, '192.168.0.211', () => {
   console.log(`Example app listening on port ${port}`)
   console.log(`http://localhost:${port}`)
 })
